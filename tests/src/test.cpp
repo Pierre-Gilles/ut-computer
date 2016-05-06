52367b15f3b78d9263189fd7097ab29d683dee8e
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-05-06 13:00:12 +0200

    delete tests files

--- tests/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "headers/test.h"
-
-void TestQString::test1(){
-    QString str = "Hello";
-    QVERIFY(str.toUpper() == "HELLO");
-}
-
-QTEST_MAIN(TestQString)
-//#include "TestQString.moc"
