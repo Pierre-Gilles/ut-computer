37ae665e48fb2b2264adc9266795a3fa8883f257
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-05-23 13:27:51 +0200

    Finish tests on StackUTComputer

--- UTComputer/ut-computer-tests/stack-tests/tests_StackUTComputer.cpp
+++ UTComputer/ut-computer-tests/stack-tests/tests_StackUTComputer.cpp
@@ -35,7 +35,6 @@ TEST_F(Test_StackUTComputer_Class, Test_top_Function) {
 
 TEST_F(Test_StackUTComputer_Class, Test_pop_Function) {
     ComplexLiteral *test = new ComplexLiteral(new NumericLiteral(2));
-    ComplexLiteral *test2 = nullptr;
     st.push(test);
     EXPECT_EQ(1, st.size());
     st.pop();
@@ -45,22 +44,36 @@ TEST_F(Test_StackUTComputer_Class, Test_pop_Function) {
 }
 
 TEST_F(Test_StackUTComputer_Class, Test_clear_Function) {
+    ComplexLiteral *test1 = new ComplexLiteral(new NumericLiteral(2));
+    ComplexLiteral *test2 = new ComplexLiteral(new NumericLiteral(2));
+    ComplexLiteral *test3 = new ComplexLiteral(new NumericLiteral(2));
+    ComplexLiteral *test4 = new ComplexLiteral(new NumericLiteral(2));
+    st.push(test1);
+    st.push(test2);
+    st.push(test3);
+    st.push(test4);
+    EXPECT_EQ(4, st.size());
+    st.clear();
+    EXPECT_EQ(0, st.size());
 
+    // expect death when accessing a deleted pointer
+    EXPECT_DEATH(test1->toString(), ".*"); // ".*" match any error code
+    EXPECT_DEATH(test2->toString(), ".*"); // ".*" match any error code
+    EXPECT_DEATH(test3->toString(), ".*"); // ".*" match any error code
+    EXPECT_DEATH(test4->toString(), ".*"); // ".*" match any error code
 }
 
 TEST_F(Test_StackUTComputer_Class, Test_getArguments_Function) {
-
     try {
         Literal **arguments = new Literal*[10];
-        st.getArguments(0, arguments); // should pass and raise no error or crash
 
-
+        // should pass and raise no error or crash
+        EXPECT_NO_THROW(st.getArguments(0, arguments));
+        EXPECT_NO_FATAL_FAILURE(st.getArguments(0, arguments));
 
         // Stack is empty, so getArguments with 2 as arity should raise exception
         //st.getArguments(2,arguments); // uncomment to get exception message
         EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
-
-
 
         // Stack.size() is two, but both arguments are null => exception raised
         st.push(nullptr);
@@ -68,7 +81,6 @@ TEST_F(Test_StackUTComputer_Class, Test_getArguments_Function) {
         // st.getArguments(2,arguments); // uncomment to get exception message
         EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
 
-
         // Stack.size() is two, but one argument is null => exception raised
         st.clear();
         st.push(new ComplexLiteral(new NumericLiteral(2)));
@@ -76,14 +88,12 @@ TEST_F(Test_StackUTComputer_Class, Test_getArguments_Function) {
         // st.getArguments(2,arguments); // uncomment to get exception message
         EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
 
-
         // Stack.size() is two, but one argument is null => exception raised
         st.clear();
         st.push(nullptr);
         st.push(new ComplexLiteral(new NumericLiteral(2)));
         // st.getArguments(2,arguments); // uncomment to get exception message
         EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
-
 
         // Stack.size() is two, and no null arguments
         st.clear();
@@ -102,46 +112,42 @@ TEST_F(Test_StackUTComputer_Class, Test_deleteArguments_Function) {
 
     try {
         Literal **arguments = new Literal*[10];
-        st.getArguments(0, arguments); // should pass and raise no error or crash
 
+        // should pass and raise no error or crash
+        EXPECT_NO_THROW(st.deleteArguments(0));
+        EXPECT_NO_FATAL_FAILURE(st.deleteArguments(0));
 
+        // Stack is empty, so deleteArguments with 2 as arity should raise exception
+        //st.deleteArguments(2); // uncomment to get exception message
+        EXPECT_THROW(st.deleteArguments(2),UTComputerException);
 
-        // Stack is empty, so getArguments with 2 as arity should raise exception
-        //st.getArguments(2,arguments); // uncomment to get exception message
-        EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
-
-
-
-        // Stack.size() is two, but both arguments are null => exception raised
+        // Stack.size() is two, but both arguments are null => no exception raised
         st.push(nullptr);
         st.push(nullptr);
-        // st.getArguments(2,arguments); // uncomment to get exception message
-        EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
+        EXPECT_NO_THROW(st.deleteArguments(2));
 
-
-        // Stack.size() is two, but one argument is null => exception raised
+        // Stack.size() is two, but one argument is null => no exception raised
         st.clear();
         st.push(new ComplexLiteral(new NumericLiteral(2)));
         st.push(nullptr);
-        // st.getArguments(2,arguments); // uncomment to get exception message
-        EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
+        EXPECT_NO_THROW(st.deleteArguments(2));
 
-
-        // Stack.size() is two, but one argument is null => exception raised
+        // Stack.size() is two, but one argument is null => no exception raised
         st.clear();
         st.push(nullptr);
         st.push(new ComplexLiteral(new NumericLiteral(2)));
-        // st.getArguments(2,arguments); // uncomment to get exception message
-        EXPECT_THROW(st.getArguments(2,arguments),UTComputerException);
-
+        EXPECT_NO_THROW(st.deleteArguments(2));
 
         // Stack.size() is two, and no null arguments
         st.clear();
-        st.push(new ComplexLiteral(new NumericLiteral(1)));
-        st.push(new ComplexLiteral(new NumericLiteral(2)));
-        EXPECT_NO_THROW(st.getArguments(2,arguments));
-        EXPECT_EQ("1", arguments[0]->toString());
-        EXPECT_EQ("2", arguments[1]->toString());
+        ComplexLiteral *test1 = new ComplexLiteral(new NumericLiteral(2));
+        ComplexLiteral *test2 = new ComplexLiteral(new NumericLiteral(2));
+        st.push(test1);
+        st.push(test2);
+        EXPECT_NO_THROW(st.deleteArguments(2));
+        // expect death when accessing a deleted pointer
+        EXPECT_DEATH(test1->toString(), ".*"); // ".*" match any error code
+        EXPECT_DEATH(test2->toString(), ".*"); // ".*" match any error code
     }
     catch (UTComputerException e) {
         cerr << e.getMessage() << endl;
