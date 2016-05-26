fa68cc5da38ffa16a63cc264157ff7c404a6a5a8
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-05-26 08:10:08 +0200

    Add test for OperatorInferiorEqual

---
+++ UTComputer/ut_computer_tests/operators_tests/logical_operators_tests/tests_OperatorInferiorEqual.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "../../../operators/logical_operators/OperatorInferiorEqual.h"
+
+using namespace std;
+
+class Test_Operator_InferiorEqual : public ::testing::Test {
+protected:
+    StackUTComputer st;
+    OperatorInferiorEqual op_inf_eq;
+
+public:
+    virtual void SetUp() { }
+    virtual void TearDown() { }
+};
+
+
+/* ========================================================== */
+/*                  test between two integers                 */
+/* ========================================================== */
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Inf_Integer_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Sup_Integer_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Equal_Integer_Gives_True) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+
+
+/* ========================================================== */
+/*                  test between two real                     */
+/* ========================================================== */
+
+TEST_F(Test_Operator_InferiorEqual, Real_Inf_Real_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2.1))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.1))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Real_Sup_Real_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.1))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2.1))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Real_Equal_Real_Gives_True) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.1))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.1))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+
+
+
+/* ========================================================== */
+/*                  test between two rational                 */
+/* ========================================================== */
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Inf_Rational_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(7,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Sup_Rational_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(7,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Equal_Rational_Gives_True) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+
+
+/* ========================================================== */
+/*         test between integers and real / rational         */
+/* ========================================================== */
+
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Inf_Real_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Sup_Real_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2.2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Inf_Rational_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(7,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Integer_Sup_Rational_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(1,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+
+
+/* ========================================================== */
+/*         test between real and integer / rational           */
+/* ========================================================== */
+
+TEST_F(Test_Operator_InferiorEqual, Real_Inf_Integer_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2.2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Real_Sup_Integer_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Real_Inf_Rational_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(2.2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(7,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Real_Sup_Rational_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(3.2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(1,2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+
+
+/* ========================================================== */
+/*         test between rational and integer / real           */
+/* ========================================================== */
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Inf_Integer_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(4))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Sup_Integer_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(1))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Inf_Real_Gives_True) {
+    /* integer < integer = 1 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(7.2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("1", st.top()->toString());
+}
+
+TEST_F(Test_Operator_InferiorEqual, Rational_Sup_Real_Gives_False) {
+    /* integer > integer = 0 */
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(5,2))));
+    st.push(shared_ptr<ComplexLiteral>(new ComplexLiteral(NumericLiteral(1.2))));
+    op_inf_eq.execute(&st);
+    EXPECT_EQ(1, st.size());
+    EXPECT_EQ("0", st.top()->toString());
+}
