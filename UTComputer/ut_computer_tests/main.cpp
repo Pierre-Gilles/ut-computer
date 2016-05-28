024c9dfcdaeede7f09e84b3f941609b46b3bb22c
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-05-28 19:17:27 +0200

    Add tests for operators DUP and CLEAR

--- UTComputer/ut_computer_tests/main.cpp
+++ UTComputer/ut_computer_tests/main.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
-//    ::testing::GTEST_FLAG(filter) = "Test_Operator_RE*:";
+//    ::testing::GTEST_FLAG(filter) = "Test_Operator_CLEAR*:";
     //::testing::GTEST_FLAG(filter) += "Test_Operator_Minus*:";
     return RUN_ALL_TESTS();
 }
 
-// TODO tests on operator DUP, DROP, SWAP, LASTOP, UNDO, REDO, LASTARGS, CLEAR
+// TODO tests on operator DUP, SWAP, LASTOP, UNDO, REDO, LASTARGS
