3005244fb7e0f4b8c1c9b330d4dcefe6e2728ea9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-06-12 17:32:37 +0200

    doxygen documentation

--- UTComputer/views/atomdialog.h
+++ UTComputer/views/atomdialog.h
@@ -20,19 +20,46 @@ public:
     ~AtomDialog();
 
 private slots:
+
+    /**
+     * \fn on_pushButtonCreateAtom_clicked
+     * \brief Triggered when the createAtom button is clicked
+     */
     void on_pushButtonCreateAtom_clicked();
 
+    /**
+     * \fn on_textEdit_textChanged
+     * \brief Triggered when the text changed
+     */
     void on_textEdit_textChanged();
 
+    /**
+     * \fn on_pushButtonDeleteAtom_clicked
+     * \brief Triggered when the delete button is clicked
+     */
     void on_pushButtonDeleteAtom_clicked();
 
+    /**
+     * \fn on_listWidget_itemSelectionChanged
+     * \brief Triggered when the selection changed
+     */
     void on_listWidget_itemSelectionChanged();
 
 private:
     Ui::AtomDialog *ui;
     Calculator *calc;
     vector<vector<string>> atoms;
+
+    /**
+     * \fn updateList
+     * \brief Update the atom list
+     */
     void updateList();
+
+    /**
+     * \fn displayError
+     * \brief Display an error
+     */
     void displayError(string e);
 };
 
